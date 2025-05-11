6cb39060aec68b585a749993e6664516886e55c2
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2025-05-11 19:57:50 +0100

    update friends

--- src/apis/fetchFriends.ts
+++ src/apis/fetchFriends.ts
@@ -4,33 +4,33 @@ import { getUsersUseridFriendsFind } from "rozod/lib/endpoints/friendsv1";
 type response = ExtractResponse<typeof getUsersUseridFriendsFind>;
 
 export const fetchFriends = async (
-	userId: number,
-	onPage?: (batch: number[]) => void,
+  userId: number,
+  onPage?: (batch: number[]) => void,
 ) => {
-	let cursor: string | undefined = undefined;
+  let cursor: string | undefined = undefined;
 
-	do {
-		const request = {
-			userId,
-			limit: 50,
-		} as {
-			userId: number;
-			limit: number;
-			cursor?: string;
-		};
+  do {
+    const request = {
+      userId,
+      limit: 50,
+    } as {
+      userId: number;
+      limit: number;
+      cursor?: string;
+    };
 
-		if (cursor) {
-			request.cursor = cursor;
-		}
-		const response: response = await fetchApi(
-			getUsersUseridFriendsFind,
-			request,
-		);
-		const { PageItems: friends } = response;
+    if (cursor) {
+      request.cursor = cursor;
+    }
+    const response: response = await fetchApi(
+      getUsersUseridFriendsFind,
+      request,
+    );
+    const { PageItems: friends } = response;
 
-		if (onPage) {
-			onPage(friends.map((friend) => friend.id));
-		}
-		cursor = response.NextCursor;
-	} while (cursor);
+    if (onPage) {
+      onPage(friends.map((friend) => friend.id));
+    }
+    cursor = response.NextCursor;
+  } while (cursor);
 };
